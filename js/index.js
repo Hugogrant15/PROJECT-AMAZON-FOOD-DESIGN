9a3a31271ade2aa04d13a9b9a53263aad748fd3a
Hugogrant15 <ugochukwuomenogor@gmail.com>
2025-08-20 17:20:21 +0100

    new commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+
+function loginButton(event) {
+     event.preventDefault()
+     const spinItem = document.querySelector(".spin");
+    spinItem.style.display = "inline-block";
+
+     const getEmail = document.getElementById("email").value;
+    const getPassword = document.getElementById("password").value;
+
+    if(getEmail === "" || getPassword === "") {
+          Swal.fire({
+          icon: 'info',
+          text: 'All fields are required!',
+          confirmButtonColor: "#00A859"
+        })
+        spinItem.style.display = "none";
+     //    return;
+    }
+}
+
+
+
+
+
+
 function showProducts(event) {
      location.href = "product-page.html"
 }
